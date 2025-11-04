31d97ab2baad10c984dbead2ed6126c17e798be4
MichaLademann <mic.lademann81@gmail.com>
2025-11-04 11:04:35 +0100

    dalsza praca

--- src/Directory.jsx
+++ src/Directory.jsx
@@ -1,21 +1,12 @@
-import Details from "./Details"
-
-function Directory({ name, type, children }) {
-
+function Directory(name, type, children) {
     return (
         <>
-            {/* Wyświetl szczegóły pliku/folderu takie jak nazwa i typ */}
-            <Details name={name} type={type} />
-            {
-                // jeśli napotkany plik będzie typu "folder", to znaczy,
-                // że ma dzieci - wyświetli je za pomocą funkcji .map()
-                type === "folder" &&
-                // jeśli typ będzie inny, ten kod się nie wykona.
-                children.map(
-                    (child) => <Directory key={child.id} name={child.name} type={child.type} children={child.children} />
-                )
-            }
-        </>
+                    <p>{x.name}, {x.type}</p>
+                    {x.children.map(
+                        (child) => <Directory name={child.name} type={child.type} children={child.children}></Directory>
+
+                    )}
+                </>
     )
 }
 
